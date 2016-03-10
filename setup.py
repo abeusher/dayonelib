4df134f601abcb033ec04cf7596f25ee25d44661
Phil Jackson <github@underscorephil.com>
2016-03-10 13:30:32 -0600

    bumping version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='dayonelib',
-      version='0.1',
+      version='0.2',
       packages=['dayonelib'],
       description='Tool for DayOne entry automation',
       author='Phil Jackson',
       author_email='opensource@underscorephil.com',
       url='https://www.github.com/underscorephil/dayonelib/',
-      download_url = 'https://github.com/underscorephil/dayonelib/tarball/0.1',
+      download_url='https://github.com/underscorephil/dayonelib/tarball/0.2',
       license='MIT License',
       keywords=['dayone']
       
